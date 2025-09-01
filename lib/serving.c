4bf92451346a08cdf5f3dfbca68a3cd1e3b82445
Pedro Miguel <pedro.miguel.e.idk@gmail.com>
2025-08-31 21:02:39 -0300

    chore: fix spacing

--- lib/serving.c
+++ lib/serving.c
@@ -153,34 +153,34 @@ int __request_read(int connection_fd, Chaining ** buffer) {
     int flags = fcntl(connection_fd, F_GETFL, 0);
     fcntl(connection_fd, F_SETFL, flags | O_NONBLOCK);
 
-        FD_ZERO(&read_fds);
-        FD_SET(connection_fd, &read_fds);
+    FD_ZERO(&read_fds);
+    FD_SET(connection_fd, &read_fds);
 
-        timeout = (struct timeval) {
-            .tv_usec = 300000
-        };
+    timeout = (struct timeval) {
+        .tv_usec = 300000
+    };
 
-        int activity = select(connection_fd + 1, &read_fds, NULL, NULL, &timeout);
+    int activity = select(connection_fd + 1, &read_fds, NULL, NULL, &timeout);
 
-        if (activity < 0) {
-            perror("select error");
-        } else if (activity == 0) {
-            printf("Timeout occurred, no data available.\n");
-        } else if (activity) {
-            do {
-                bytes = recv(connection_fd, packet, sizeof(packet), MSG_DONTWAIT);
-                if (bytes > 0) {
-                    Chaining_append_raw_arena(&Raw_request_arena, buffer, packet, bytes);
+    if (activity < 0) {
+        perror("select error");
+    } else if (activity == 0) {
+        printf("Timeout occurred, no data available.\n");
+    } else if (activity) {
+        do {
+            bytes = recv(connection_fd, packet, sizeof(packet), MSG_DONTWAIT);
+            if (bytes > 0) {
+                Chaining_append_raw_arena(&Raw_request_arena, buffer, packet, bytes);
+            } else {
+                if (errno == EAGAIN && errno == EWOULDBLOCK) {
+                    break;
                 } else {
-                    if (errno == EAGAIN && errno == EWOULDBLOCK) {
-                        break;
-                    } else {
-                        perror("recv error");
-                        break;
-                    }
+                    perror("recv error");
+                    break;
                 }
-            } while (1);
-        }
+            }
+        } while (1);
+    }
     Chaining_print(*buffer);
     return 0;
 }
